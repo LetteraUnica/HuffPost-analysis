9b07f56ae5880cf69dded02ade4cd8f49b74aee4
LetteraUnica <lorenzocav97@gmail.com>
2022-05-28 18:27:48 +0200

    Update tokenizer.py

--- final-project/tokenizer.py
+++ final-project/tokenizer.py
@@ -1,20 +1,22 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from copy import deepcopy
+
+
 class MyTokenizer:
     def __init__(self, lowercase=True, min_df=0.001, max_df=1.):
         self.count_vectorizer = CountVectorizer(lowercase=lowercase,
-                                                min_df=min_df, 
-                                                max_df=max_df, 
+                                                min_df=min_df,
+                                                max_df=max_df,
                                                 ngram_range=(1, 1))
 
-    def convert2ints(self, document, word2int):
+    def convert2ints(self, document):
         res = []
         for word in document.split(" "):
             word = word.strip()
             if word in word2int:
-                res.append(word2int[word])
+                res.append(self.word2int[word])
             else:
-                res.append(word2int[self.UNK])
+                res.append(self.word2int[self.UNK])
         return res
 
     def fit(self, X):
@@ -30,11 +32,12 @@ class MyTokenizer:
         self.word2int = word2int
 
     def transform(self, X):
-        return [self.convert2ints(document, self.word2int) for document in X]
+        return [self.convert2ints(document) for document in X]
 
     def get_vocab_size(self):
         return len(self.word2int)
 
-    def pad_to_max_length(tokenized_documents, padding_token):
-        max_length = max([len(document) for document in tokenized_documents])
-        return [document + [padding_token]*(max_length-len(document)) for document in tokenized_documents] 
+
+def pad_to_max_length(tokenized_documents, padding_token):
+    max_length = max([len(document) for document in tokenized_documents])
+    return [document + [padding_token]*(max_length-len(document)) for document in tokenized_documents]
